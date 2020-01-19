ccd3a966b447b9c88f128f486739e60d8b01aa10
Samuel Knight <knight.samuel@outlook.com>
2020-01-19 19:30:52 +0100

    Added first two labs

---
+++ lab2/README.md
@@ -0,0 +1,2 @@
+# Lab 2
+Lab involving reading from onboard accelerometer
---
+++ lab2/main.cpp
@@ -0,0 +1,14 @@
+#include "mbed.h"
+
+
+Serial pc(USBTX, USBRX);
+
+// Initialize accelerometer with i2c
+
+int main()
+{
+    printf("Begin reading data from accelerometer...\r\n\r\n");
+    ThisThread::sleep_for(500);
+    
+    // Loop: reading from accelerometer
+}
